68d7c9ef504576e673aeec1e4c762efab938da77
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2018-03-24 23:30:24 -0400

    start melt functions

---
+++ R/melt-cces.R
@@ -0,0 +1,20 @@
+# melt wide cces (raw versions) to correct var names
+# make common function
+melt_cces <- function(tbl, q_expr){
+
+  # drop unnecessary columns and manipulate
+  rc <- select(tbl, case_id, year, matches(q_expr)) %>%
+    mutate_at(vars(matches(q_expr)), as_factor) %>% # make labels from dta info
+    mutate_at(vars(matches(q_expr)), as.character) # make character so we can bind without error
+
+  # MELT
+  rc.long <- melt(as.data.table(rc),
+                  id.vars = c("case_id", "year"),
+                  value.name = "response",
+                  variable.name = "question",
+                  variable.factor = FALSE,
+                  value.factor = FALSE)
+
+  return(rc.long)
+}
+

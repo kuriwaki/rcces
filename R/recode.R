34edc0d7f7dd33ab9031d93e3d08ba101442b371
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2018-05-27 07:28:01 +0900

    standardize varname

---
+++ R/recode.R
@@ -0,0 +1,26 @@
+#' Standardize party codes
+#'
+#' @param x A vector of characters with unstandardized party, e.g. "Republican", "Not sure"..
+#'
+#' @return A vector of "D", "R",
+#'
+#' @examples
+#' std_short_party(c("Republican", "Democrat", "Not sure", "Other Party / Independent", "Never Heard of Person")
+#'
+std_short_party <- function(x) {
+  dplyr::recode(x,
+         Democrat = "D",
+         Republican = "R",
+         Independent = "I",
+         `Never Heard of Person` = "NeverHeard",
+         `Not sure` = "NotSure",
+         `Not Sure` = "NotSure",
+         `Don't know` = "NotSure",
+         `Other Party / Independent` = "OtherI",
+         `Other Party/Independent` = "OtherI",
+         `Skipped` = "",
+         `skipped` = "",
+         `Not Asked` = ""
+  )
+}
+
---
+++ R/standardize-varname.R
@@ -0,0 +1,40 @@
+#' Standardize name of party perception variable across different CCES datasets
+#'
+#' Relies on having the office
+#'
+#' @param df CCES dataframe with multiple "perceived vote" questions. Slimmed.
+#' @param prefix
+#'
+#' @return  The same df but with variables renamed with suffixes.
+std_party_prcp_varname <- function(df, prefix =  "party_prcp_") {
+
+  vt <- vartab(df)
+
+  # get alias of matching
+  rep_var <-   filter(vt, str_detect(label, "(Rep|House)")) %>% pull(alias)
+  if (length(rep_var) > 1) {
+    rep_var <- filter(vt, str_detect(label, "(House)")) %>% pull(alias)
+  }
+  sen1_var <-  filter(vt, str_detect(label, "Sen(ator\\s|)1")) %>% pull(alias)
+  sen2_var <-  filter(vt, str_detect(label, "Sen(ator\\s|)2")) %>% pull(alias)
+  gov_var <-   filter(vt, str_detect(label, "Gov(ernor|)")) %>% pull(alias)
+
+  if (any(length(rep_var) != 1,
+          length(sen1_var) != 1,
+          length(sen2_var) != 1,
+          length(gov_var) != 1))  {
+    stop ("Choose better regex to pinpoint column")
+  }
+
+  rep_new  <- str_c(prefix, "rep")
+  sen1_new <- str_c(prefix, "sen1")
+  sen2_new <- str_c(prefix, "sen2")
+  gov_new  <- str_c(prefix, "gov")
+
+  df %>%
+    rename(!!rep_new := !!rep_var,
+           !!sen1_new := !!sen1_var,
+           !!sen2_new := !!sen2_var,
+           !!gov_new := !!gov_var) %>%
+    mutate_at(vars(matches("prcp")), function(x) as.character(as_factor(x)))
+}

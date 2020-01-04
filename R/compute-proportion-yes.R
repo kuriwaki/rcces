21b699331db4093ea393a917460ba05a5fb8454d
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2020-01-04 14:32:46 -0500

    snake case, pool to single function

--- R/compute-proportion-yes.R
+++ R/compute-proportion-yes.R
@@ -1,36 +1,23 @@
 # function to compute proportion Yes on  any constituency
 
-#' Proportion Yes by question
-#'
-#' Splits by qID, or a question asked in a given year. Does not aggregate across years
+#' Computation underlying ques_split and issue_split
 #'
 #' @rdname split
 #'
-#' @param tbl The dataset with the columns \code{cong}, \code{question}
-#' @param ... variables to group on in addition to year and question, tidyeval variables
 #' @export
-question_split <- function(tbl, ...) {
-
-  var <- quos(...)
-
-  groupby <- quos(year, cong, qID, q_label, !!!var)
-
-  grp.tbl <- tbl %>%
-    filter(!is.na(response)) %>% # usually response NA means the question was not asked, so they should be dropped rather than not counted in the numerator
-    group_by(!!! groupby) %>%
-    summarise(wgtYes = sum(weight * (response == "Y")), # Y's get 1, rest 0. inner product with weight. Sum.
-              wgtNo = sum(weight * (response == "N")),
-              wgtDK = sum(weight * (response == "DK")),
-              wgtN = sum(weight),
-              rawN = n()) %>%
+do_split <- function(grptbl) {
+  grptbl %>%
+  summarise(wgt_yes = sum(weight * (response == "Y")), # Y's get 1, rest 0. inner product with weight. Sum.
+            wgt_no  = sum(weight * (response == "N")),
+            wgt_DK  = sum(weight * (response == "DK")),
+            raw_yes = sum(response == "Y"),
+            raw_no = sum(response == "N"),
+            wgt_N   = sum(weight),
+            raw_N   = n()) %>%
     ungroup() %>%
-    collect(n = Inf) %>%
-    mutate(pctY = wgtYes / wgtN,
-           pctY2 = wgtYes / (wgtYes + wgtNo)) # 2-party vote idea
-
-grp.tbl
+    mutate(pct_yes = wgt_yes / wgt_N,
+           pct_yes2 = wgt_yes / (wgt_yes + wgt_no))
 }
-
 
 #' Proportion Yes by topic
 #'
@@ -49,19 +36,35 @@ issue_split <- function(tbl, ...) {
   var <- quos(...)
   groupby <- quos(q_label, !!!var)
 
+  # usually response NA means the question was not asked, so they should be
+  # dropped rather than not counted in the numerator
 
   grp.tbl <- tbl %>%
-    filter(!is.na(response)) %>% # usually response NA means the question was not asked, so they should be dropped rather than not counted in the numerator
+    filter(!is.na(response)) %>%
     group_by(!!! groupby) %>%
-    summarise(wgtYes = sum(weight * (response == "Y")), # Y's get 1, rest 0. inner product with weight. Sum.
-              wgtNo = sum(weight * (response == "N")),
-              wgtDK = sum(weight * (response == "DK")),
-              wgtN = sum(weight),
-              rawN = n()) %>%
-    ungroup() %>%
-    collect(n = Inf) %>%
-    mutate(pctY = wgtYes / wgtN,
-           pctY2 = wgtYes / (wgtYes + wgtNo)) # 2-party vote idea
+    do_split()
+
+  grp.tbl
+}
+
+#' Proportion Yes by question
+#'
+#' Splits by qID, or a question asked in a given year. Does not aggregate across years
+#'
+#' @rdname split
+#'
+#' @param tbl The dataset with the columns \code{cong}, \code{question}
+#' @param ... variables to group on in addition to year and question, tidyeval variables
+#' @export
+question_split <- function(tbl, ...) {
+
+  var <- quos(...)
+  groupby <- quos(q_label, year, cong, qID, !!!var)
+
+  grp.tbl <- tbl %>%
+    filter(!is.na(response)) %>%
+    group_by(!!! groupby) %>%
+    do_split()
 
   grp.tbl
 }

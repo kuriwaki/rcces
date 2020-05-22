9a28332e435f57d014e3cbbec6073eed77da1698
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2020-05-22 13:32:26 -0400

    move bin age to cceMRPprep

--- R/recode.R
+++ R/recode.R
@@ -27,28 +27,3 @@ std_short_party <- function(x) {
   )
 }
 
-
-
-#' Discretize a vector of age integers into labelled variables
-#'
-#' @param agevec a vector of integers
-#' @param agelbl a value-key pair to be passed into recode,
-#'  with values as the things to be recoded and labels as the labels for each
-#'  value.
-#'
-#' @importFrom tibble deframe
-#' @examples
-#'   ccc_bin_age(c(15:100, NA))
-#'
-#' @export
-ccc_bin_age <- function(agevec,
-                        agelbl = deframe(age5_key)) {
-  int_bin <- case_when(agevec %in% 18:24 ~ 1L,
-                       agevec %in% 25:34 ~ 2L,
-                       agevec %in% 35:44 ~ 3L,
-                       agevec %in% 45:64 ~ 4L,
-                       agevec >=   65    ~ 5L,
-                       TRUE ~ NA_integer_)
-
-  haven::labelled(int_bin, agelbl)
-}

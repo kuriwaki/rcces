8bf26690af9efb4329db93f61c91e7cb2bf54bf1
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2023-06-21 09:13:49 -0400

    Do not load all databases

--- R/database-connections.R
+++ R/database-connections.R
@@ -1,20 +1,20 @@
+#' Set up Database connections
+#' @import DBI
+#' @import RSQLite
+#' @import dbplyr
+#' @import dplyr
+setup_db <- function() {
+  con <- dbConnect(SQLite(), "~/Dropbox/CCES_representation/data/database/db.sqlite3")
+  initExtension(con)
+  src <- src_dbi(con)
 
-library(DBI)
-library(RSQLite)
-library(dbplyr)
-library(dplyr)
-
-con <- dbConnect(SQLite(), "~/Dropbox/CCES_representation/data/database/db.sqlite3")
-initExtension(con)
-src <- src_dbi(con)
-
-respn <- tbl(src, "responses")
-agrmt <- tbl(src, "agreement")
-person <- tbl(src, "person")
-legis <- tbl(src, "legis")
-votes <- tbl(src, "votes")
-qID <- tbl(src, "qID")
-qID_df <- collect(qID)
-distr <- tbl(src, "distr")
-splts <- tbl(src, "splits")
-
+  respn <- tbl(src, "responses")
+  agrmt <- tbl(src, "agreement")
+  person <- tbl(src, "person")
+  legis <- tbl(src, "legis")
+  votes <- tbl(src, "votes")
+  qID <- tbl(src, "qID")
+  qID_df <- collect(qID)
+  distr <- tbl(src, "distr")
+  splts <- tbl(src, "splits")
+}

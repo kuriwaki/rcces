1b39f9ac1bf6cbdbf8e6802d2f1249828e47cc3c
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2024-06-27 19:27:50 -0400

    Double the variance?

--- R/compute-seats.R
+++ R/compute-seats.R
@@ -7,7 +7,7 @@ prop_seats <- function(tbl, pct_var = pct_yes2_raw, n_var = n_raw, group_var = q
     mutate(sdest = sqrt({{pct_var}}*(1 - {{pct_var}})/{{n_var}})) |>
     summarize(
       prop_dists_supp_dir = mean({{pct_var}} > 0.5),
-      prop_dists_supp = mean(pnorm((0.5 - {{pct_var}})/sdest, lower.tail = FALSE)),
+      prop_dists_supp = mean(2*pnorm((0.5 - {{pct_var}})/sdest, lower.tail = FALSE)),
       avg_dist_supp = mean({{pct_var}}),
       sd_dists_supp = sd({{pct_var}}),
       n_dists = n(),

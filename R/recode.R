6526da2f911a5cb6863c98400286690fdee3f048
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2024-10-07 18:43:49 -0400

    H/S recodes

--- R/recode.R
+++ R/recode.R
@@ -1,3 +1,43 @@
+#' Recode H/S
+#'
+#' A convenience function to recode H to House (or CDs) and S to Senate
+#'
+#' @importFrom dplyr case_match
+#' @examples
+#' library(tibble)
+#' library(dplyr)
+#' library(ggplot2)
+#'
+#'
+#' tibble(chamber = c("H", "S")) |>
+#'  ggplot() +
+#'  geom_text(aes(label = chamber), x = 0.5, y = 0.5) +
+#'  facet_wrap(~ chamber, labeller = labeller(chamber = H_to_house))
+#'
+#' tibble(chamber = c("H", "S")) |>
+#'  ggplot() +
+#'  geom_text(aes(label = chamber), x = 0.5, y = 0.5) +
+#'  facet_wrap(~ chamber, labeller = labeller(chamber = H_to_cd))
+#'
+#' @export
+H_to_house <- function(x) {
+  case_match(
+    x,
+    "H" ~ "House",
+    "S" ~ "Senate",
+  )
+}
+
+#' @rdname H_to_house
+#' @export
+H_to_cd <- function(x, h_to = "Congressional Districts", s_to = "States") {
+  case_match(
+    x,
+    "H" ~ h_to,
+    "S" ~ s_to,
+  )
+}
+
 #' Standardize party codes to single capital letters
 #'
 #' @param x A vector of characters with unstandardized party, e.g. "Republican", "Not sure"..
